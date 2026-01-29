b63973733ab75e65dee0e82ba32211f0f7d10345
CryptoClock <bigboxthailand.official@gmail.com>
2026-01-29 08:13:34 +0700

    Cleanup: remove .vercel from git index

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_BonoYhu0.mjs';
-export { renderers } from '../renderers.mjs';
--- .vercel/output/static/_astro/supabase.B9bYqD4q.js
+++
@@ -1,1 +0,0 @@
-import{c as s}from"./index.DR2MMlUt.js";const a="https://euavftppzicwjhbugiys.supabase.co",p="sb_publishable_poJ-NobQZpARz_G4cWG96Q_vxIPZkrE",t=s(a,p);export{t as s};
